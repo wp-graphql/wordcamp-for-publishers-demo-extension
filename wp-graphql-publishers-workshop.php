31c5a8e342b03c3ddea7671bfe648023f2d3dd58
jasonbahl <jasonbahl@mac.com>
2017-08-11 13:24:53 -0600

    Add a new RootQuery field `wordCampRocks` to show how fields can be added to the WPGraphQL Schema.

--- wp-graphql-publishers-workshop.php
+++ wp-graphql-publishers-workshop.php
@@ -4,3 +4,32 @@
  * Description: This plugin was created for the 2017 WordCamp for Publishers conference and is not intended for production use.
  * Author: WPGraphQL, Jason Bahl
  */
+
+/**
+ * Add a new root query entry point.
+ *
+ * This can be queried like so:
+ *
+ * {
+ *   wordCampRocks
+ * }
+ *
+ * and will return the following JSON:
+ *
+ * [
+ *   data => [
+ *     wordCampRocks: 'Yes, it does'
+ *   ]
+ * ]
+ *
+ */
+add_action( 'graphql_root_queries', function( $fields ) {
+	$fields['wordCampRocks'] = [
+		'type' => \WPGraphQL\Types::string(),
+		'description' => __( 'An example field showing how to add to the root schema', 'wp-graphql-publishers' ),
+		'resolve' => function() {
+			return 'Yes, it does';
+		},
+	];
+	return $fields;
+} );
