494c78edd601aaaab2d5f345802a2a74fb5b11c8
jasonbahl <jasonbahl@mac.com>
2017-08-11 13:47:19 -0600

    Add a new “color” field to the “post” schema that resolves to the value of the “color” postmeta field.

--- wp-graphql-publishers-workshop.php
+++ wp-graphql-publishers-workshop.php
@@ -33,3 +33,40 @@ add_action( 'graphql_root_queries', function( $fields ) {
 	];
 	return $fields;
 } );
+
+/**
+ * Add a new "color" field to the "post" schema.
+ *
+ * This can be queried like so (for the post with a database id 1):
+ * NOTE: "cG9zdDox" = base64_encode( 'post:1' );
+ *
+ * {
+ *   post(id:"cG9zdDox") {
+ *     title
+ *     color
+ *   }
+ * }
+ *
+ * Assuming the post being queried had a postmeta field "color" with a value of "blue"
+ * This will return:
+ *
+ * [
+ *   data => [
+ *     post => [
+ *       title => 'Hello World',
+ *       color => 'blue'
+ *     ]
+ *   ]
+ * ]
+ *
+ */
+add_action( 'graphql_post_fields', function( $fields ) {
+	$fields['color'] = [
+		'type' => \WPGraphQL\Types::string(),
+		'description' => __( 'An example showing how to add a field to the "post" schema', 'wp-graphql-publishers' ),
+		'resolve' => function( \WP_Post $post ) {
+			return get_post_meta( $post->ID, 'color', true );
+		},
+	];
+	return $fields;
+}, 10, 1);
